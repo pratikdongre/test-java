df473a8f0b3a1b974742cfaeffb17e23ff23a5c1
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-09-07 19:00:07 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,10 @@
-//TEST
+public class test {
+    public static void main(String[] args) {
+        int firstnum = 1;
+        int secondnum = 2;
+        System.out.println(firstnum+secondnum);
+
+        
+    }
+    
+}
