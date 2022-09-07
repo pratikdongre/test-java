420868fcce2e0ee6c8a74e4d3744ade9df0b1aea
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-09-07 19:12:40 +0900

    Create javaproject2022.java

---
+++ javaproject2022.java
@@ -0,0 +1,29 @@
+public class javaproject2022 {
+    public static void main(String[] args) {
+        // 1)Declare
+        // 2)Data types
+        // 3)visibility
+        // 4)Life cycle
+
+        // "자바"라는 컴파일러 언어는 매우 딱딱함(하나하나씩 다 선언을 해줘야함)
+        // 세미콜론이 붙는 이유는 구분을 시킬려고 하는 것이다
+        //그러므로 알아보기가 쉽다는 장점이 있다
+
+        // 변수이름을 짓는 이유: 구분을 지어주기 위해서 이다
+        /* <데이터 타입을 선언하는 이유>
+         * 자료형 마다 메모리 저장 방식이 다르기때문이다
+         * 메모리를 효율적으로 사용하기 때문이다
+         */
+
+
+         System.out.println("Hello world");// 이 입력값을 실행하면 Hello world
+
+
+         int firstnum = 5; 
+         int secondnum = 6;
+         int finalnum = firstnum + secondnum; // 5와6을 더해준다
+         System.out.println(finalnum); // 11
+        
+    }
+    
+}
