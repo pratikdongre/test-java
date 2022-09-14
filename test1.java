23b1704ce84f358318f82e8301b68050412c7b2b
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-09-14 16:17:51 +0900

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 public class test1 {
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);// 삼각형 
+        Scanner scn = new Scanner(System.in);
       
-      double firstValue = a.nextDouble();
-      double secondtValue = a.nextDouble();
+      double fisrtValue = scn.nextDouble();
+      double secondValue = scn.nextDouble();
 
-      System.out.println(0.5*(firstValue*secondtValue));//삼각형 면적 구하기 공식 
+      System.out.println(0.5*(fisrtValue*secondValue));
+      //삼각형 면적 구하기 공식(2분의1 * 밑변 * 높이)
     }
 }
