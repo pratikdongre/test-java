0a66e97bf78ed3160b701e692d13ee96a274b063
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-09-14 16:00:43 +0900

    Create javaproject2022.java

---
+++ javaproject1/javaproject2022.java
@@ -0,0 +1,27 @@
+package javaproject1;
+
+import java.util.Scanner;
+
+public class javaproject2022 {
+
+	public static void main(String[] args) {
+			
+		Scanner scn = new Scanner(System.in);
+	      //삼각형 면적 구하기 (공식: 0.5*밑변*높이)
+	      
+		
+		  double firstValue; // 밑변 
+	      double secondValue; // 높이
+	      
+	      firstValue = scn.nextDouble(); 
+	      secondValue = scn.nextDouble();
+	      
+	      System.out.println(0.5*(firstValue*secondValue));
+	      // 삼각형 면적 공식이다 (2분의1(0.5)를 곱해준다)
+	       
+	      
+	      
+		     
+	} 
+
+}
