fa0914ca9469d07724e7f54204ff456f3e526a30
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-09-21 10:50:40 +0900

    Update test4.java

--- test4.java
+++ test4.java
@@ -2,7 +2,7 @@ public class test4 {
     public static void main(String[] args) {
         // 상수 
         // 변하지 않는 값
-        int a = 1; // 여기에서 1이 상수를 의미한다
+       final int a = 1; // 여기에서 1이 상수를 의미한다
 
         1=2; 
         1=3; // 이와 같이 봤을때 상수 값이 변하면 에러가 나온단
