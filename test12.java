4549477bd771f506d57dfefb2cca388d0c5325af
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-09-30 12:41:03 +0900

    Create test12.java

---
+++ test12.java
@@ -0,0 +1,16 @@
+public class test12 {
+    public static void main(String[] args) {
+        
+
+
+        //부호연산자 논리부정연산자
+        // int i = -10;
+
+        // i = +i;
+
+        // System.out.println(i);
+
+        // i = -i;
+        // System.out.println(i);
+    }
+}
