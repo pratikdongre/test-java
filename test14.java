4e2f766fca23d542842b7e99878d55c72eca537e
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-05 11:12:56 +0900

    Create test14.java

---
+++ test14.java
@@ -0,0 +1,24 @@
+public class test14 {
+    public static void main(String[] args) {
+        	//for 반복문 
+		//whlie 문과 같이 반복을 시켜주는 형식이다
+		//하지만 형식이 조금 다르다
+		//for(초기값;,중요조건;,반복실행){
+		//  반복실행 구문
+		
+		//}
+		//위 와 같은 형식으로 좀더 보기가 쉽고 깔끔하게 만들수 있다
+		
+		
+		for(int fisrtnum = 89; fisrtnum<700; ++fisrtnum) {
+			
+			System.out.println("안녕하세요" + fisrtnum);// 위 조건식으로 했을때는 699에서 멈춘다
+			
+			
+		}
+	}
+		
+ }
+    
+    
+
