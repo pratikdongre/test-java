c5ef10e3b0330f675ac9441e085fc59ecfb2ea2a
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-05 14:53:11 +0900

    Create test17.java

---
+++ test17.java
@@ -0,0 +1,42 @@
+public class test17 {
+    public static void main(String[] args) {
+       
+//		// 		int + flaot 
+//		// 이항 연산 시 반드시 자료형을 일치 해야된다
+//		// 언어 마다 이항 연산시 규칙이 틀리다
+//		
+//		double sum = 2+3.0f;
+//		
+//		
+		
+//		System.out.println(sum); 
+		
+//		
+//		byte a= 2;
+//		byte b= 3;
+//		
+//		byte c= a+b;
+//		  jvm 에서 두가지 타입 만듬
+// 		  int long 으로 만듬
+//		  규칙: ALU 설계 할때 두가지로 만듬
+//		  근데 설계 할때 INT 이하의 자료값은 INT값으로 해석
+//		  그래서 에러 표시가 뜬다
+			
+		//변수 ,정수
+		// int 이하의 자료형은 무조건-> int 형으로 변환
+		// 자료형의 우선 순위는 long>int
+		 
+		byte a =2;
+		long b =3;
+		
+		int c= (int)(a + b); //정수형에서 이항 연산을 처리하는 규칙 (변수인 경우)
+		
+		System.out.println(c);
+		
+	
+	
+	
+	}
+	
+		
+}
