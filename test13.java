1676f638dc87d7383d2453803877e53610df5065
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-05 11:12:01 +0900

    Create test13.java

---
+++ test13.java
@@ -0,0 +1,15 @@
+public class test13 {
+    public static void main(String[] args) {
+        	// Whlie 반복문
+		// Whlie(조건){
+		// 반복 실행 영역(반복 작업)
+		//} 기본적인 while 문의 형식이다
+		
+		while(true) {
+			System.out.println("안녕하세요"+1);
+			System.out.println("반갑습니다");// 안에 있는 출력값이 계속 반복해서 값이 출력됨
+						
+		}
+    }
+    
+}
