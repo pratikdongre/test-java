0ae2038b9095d8941d64b14017d42b65ea427ffc
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-05 15:42:18 +0900

    Create test18.java

---
+++ test18.java
@@ -0,0 +1,25 @@
+public class test18 {
+    public static void main(String[] args) {
+        	// 문자와 문자열은 엄연히 다름
+		// 문자는 : 한글자
+		// 문자열 : 한글자 이상
+		// char -> 문자형
+		// 문자열 -> String, char array
+		
+//		char temp = 'a'; // "a";
+//		
+//		String bar = "hello world";
+//		
+//		System.out.println(bar); //결론 문자열의 자료형은 String형은 사용한다
+		
+//		String bar = "11";
+//		
+//		System.out.println(bar+2.3);// 문자열은 저장할수있는 String 형이 최강이다
+// 		값이 더해져서 나오는게 아니라 붙어서 나온다
+// 		112.3
+		
+//		
+
+    }
+    
+}
