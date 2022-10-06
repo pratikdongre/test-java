930a1342a65f9f4bc4f577b6b3fa2a35613b7098
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-06 20:43:01 +0900

    Update test20.java

--- test20.java
+++ test20.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
-
-
 public class test20 {
-  
     public static void main(String[] args) {
         // 구구단 값 출력 하기
        while(true){
@@ -19,6 +16,7 @@ public class test20 {
         //1번 입력시
         if(inputValue==1){
             System.out.println("출력할 구구단의 단을 입력하세요. 구구단의 단은2~9사이");
+        while(true){// 여기에 무한 반복을 쓰는 이유는 범위가 계속 맞을때 까지 foo값을 무한으로 반복
         int foo = scan.nextInt();
         if(foo>=2 && foo<=9){
             for(int firstValue = 1 ; firstValue<=9; firstValue++){
@@ -26,11 +24,13 @@ public class test20 {
         //1번 을 입력시 구구단의 식이 나온다 ex) 3을 입력시 3의 구구단 이 나옴          
 
             }
+            break;
             
         // 그런데 2와9사이 정수를 입력 하지 않을 시에는 다음 과 같은 값이 출력됨     
          }else{
             System.out.println("2~9사이 정수를 입력해주세요"); // 2~9사이 정수를 입력해주세요
          }
+        }
 
     }    // 2번버튼을 누를시에는 프로그램이 종료가 된다
     else if(inputValue==2){
@@ -48,23 +48,5 @@ public class test20 {
      
 
 
-
-
-    
-
-
-
-
-
-            
-
-
-
-
-
-
-
-     
-    
 
 
